911ce382d7fa9a484ef56211617936cd4a41372c
jjuniorbrasil <juniorbrasilisc@gmail.com>
2025-02-07 21:42:00 -0300

    fix: rota do fetch

--- src/components/RequestForm/sendRequest.js
+++ src/components/RequestForm/sendRequest.js
@@ -1,6 +1,6 @@
 export default async function sendRequest(data) {
   try {
-    const response = await fetch("/api/requests", {
+    const response = await fetch("/requests", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
