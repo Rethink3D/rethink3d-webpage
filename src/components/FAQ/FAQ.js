4386851f233ac176d95ca714e4683edc752fd46e
jjuniorbrasil <juniorbrasilisc@gmail.com>
2025-02-11 09:47:52 -0300

    add: novas perguntas do FAQ

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -11,15 +11,10 @@ const FAQ = () => {
           <span className="bold">enviar?</span>
         </h3>
         <p>
-          Nossa equipe entrará em contato por telefone e/ou WhatsApp para
-          atender à sua solicitação.
-        </p>
-      </div>
-      <div className="faq-item">
-        <h3>Eu serei cobrado após preencher esse formulário?</h3>
-        <p>
-          De maneira alguma, até que você feche negócio com um de nossos Makers,
-          todo o processo é livre de custos.
+          Um Maker será contactado e nossa equipe levantará um orçamento
+          adequado ao seu pedido. Assim, um de nossos agentes entrará em contato
+          por telefone e/ou WhatsApp para atender à sua solicitação e explicar
+          todos os detalhes até a aquisição de seu novo produto.
         </p>
       </div>
       <div className="faq-item">
@@ -27,8 +22,16 @@ const FAQ = () => {
           O que são <span className="bold">Makers?</span>
         </h3>
         <p>
-          <strong>'Makers'</strong> são as pessoas responsáveis pelos serviços de impressão e
-          modelagem 3D. Assim como você, são clientes de nossa plataforma.
+          <strong>'Makers'</strong> são as pessoas responsáveis pelos serviços
+          de impressão e modelagem 3D. Assim como você, são clientes de nossa
+          plataforma.
+        </p>
+      </div>
+      <div className="faq-item">
+        <h3>Eu serei cobrado após preencher esse formulário?</h3>
+        <p>
+          De maneira alguma: até que você feche negócio com um de nossos Makers,
+          todo o processo é livre de custos.
         </p>
       </div>
       <div className="faq-item">
@@ -36,8 +39,9 @@ const FAQ = () => {
           O que é a <span className="bold">Rethink3D?</span>
         </h3>
         <p>
-          A <strong>Rethink3D</strong> é uma startup que visa revolucionar o mercado de impressão
-          3D, conectando as necessidades das pessoas a quem possa suprí-las.
+          A <strong>Rethink3D</strong> é uma startup que visa revolucionar o
+          mercado de impressão 3D, conectando as necessidades das pessoas a quem
+          possa suprí-las.
         </p>
       </div>
       <div className="faq-item">
@@ -52,6 +56,15 @@ const FAQ = () => {
           >
             Confira aqui!
           </a>
+        </p>
+      </div>
+      <div className="faq-item">
+        <h3>
+          Sou um <span className="bold">Maker! Como posso participar?</span>
+        </h3>
+        <p>
+          Que legal! Entre em contato conosco pelo WhatsApp ou Instagram e lhe
+          daremos todas as informações necessárias para nossa parceria.
         </p>
       </div>
     </div>
